03835f4ad1af29bade28ad0446d7a2cb89992626
johnnybeatz <92400293+johnnybeatz@users.noreply.github.com>
2024-02-25 18:02:11 +0300

    made it use Svg icon instead of png added size and color parameters

--- lib/components/myicons.dart
+++ lib/components/myicons.dart
@@ -1,12 +1,23 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class MyCustomIcon extends Icon {
+class MyCustomIcon extends StatelessWidget {
   final String imagePath;
+  final Color color;
+  final double width; 
+  final double height; 
 
-  MyCustomIcon(this.imagePath) : super(Icons.ac_unit);
+  MyCustomIcon(this.imagePath, {this.color = Colors.grey,this.width = 20,this.height = 20}); // Default color
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(imagePath);
+    return SvgPicture.asset(
+      imagePath,
+      // ignore: deprecated_member_use
+      color: color,
+      width: width,
+      height: height,
+       // Colorize using provided color
+    );
   }
 }
