6e5c83cb310907a0b7c1e917d93f9da10ecb2d3c
johnnybeatz <92400293+johnnybeatz@users.noreply.github.com>
2024-02-25 18:01:18 +0300

    removed unnecessary class

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'index.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,22 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Musica',
-        home: MyScaffold());
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
         darkTheme: darkTheme(),
-        debugShowCheckedModeBanner: false, // Ensure MaterialApp is present
-        theme: darkTheme(),
+        title: 'Musica',
         home: Homepage());
   }
 }
+
