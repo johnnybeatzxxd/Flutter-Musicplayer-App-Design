bd443d7fe9b73688a984f7303d618290293a8c5e
johnnybeatz <92400293+johnnybeatz@users.noreply.github.com>
2024-02-25 11:42:33 +0300

    changed the title of the app

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Musica',
         home: MyScaffold());
   }
 }
