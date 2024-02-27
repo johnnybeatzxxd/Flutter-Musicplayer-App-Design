d39962199114055262a7be3af1e72c6aa296446d
johnnybeatz <johnnybeatzxxd@gmail.com>
2024-02-27 20:00:56 +0300

    component and pages added to the index file

--- lib/index.dart
+++ lib/index.dart
@@ -1,3 +1,6 @@
 export 'Pages/home.dart';
 export 'Theme/theme.dart';
 export 'Pages/favourite.dart';
+export 'Pages/playground.dart';
+export 'components/myicons.dart';
+export 'components/buttomnavbar.dart';
