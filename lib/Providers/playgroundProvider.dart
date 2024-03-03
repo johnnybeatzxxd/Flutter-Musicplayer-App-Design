003f8612706afc2ab168352a1e4e6c30d4388039
johnnybeatz <johnnybeatzxxd@gmail.com>
2024-03-03 18:58:41 +0300

    added a function that calculates the slider value to time format

--- lib/Providers/playgroundProvider.dart
+++ lib/Providers/playgroundProvider.dart
@@ -6,6 +6,8 @@ class playGroundProvider extends ChangeNotifier {
   get isplay => _isplay;
   double _slider = 0.0;
   get slider => _slider;
+  double _maxSlider = 120;
+  get maxSlider => _maxSlider;
 
   changePlayButtonIcon() {
     _isplay = !_isplay;
@@ -15,5 +17,25 @@ class playGroundProvider extends ChangeNotifier {
   setSlider(value){
     _slider = value;
     notifyListeners();
+  }
+
+  setMaxSlider(value){
+    _maxSlider = value;
+    notifyListeners();
+  }
+  String intToTime(int value) {
+    int h, m, s;
+    h = value ~/ 3600;
+    m = ((value - h * 3600)) ~/ 60;
+    s = value - (h * 3600) - (m * 60);
+
+    String hourLeft = h.toString().length < 2 ? "0" + h.toString() : h.toString();
+    String minuteLeft =
+        m.toString().length < 2 ? "0" + m.toString() : m.toString();
+
+    String secondsLeft =
+        s.toString().length < 2 ? "0" + s.toString() : s.toString();
+    String result = h > 0 ? "$hourLeft:$minuteLeft:$secondsLeft" : "$minuteLeft:$secondsLeft";
+    return result;
   }
 }
