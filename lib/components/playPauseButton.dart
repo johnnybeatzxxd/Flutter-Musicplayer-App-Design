adb8a878729c802daafcc9c1c1de664c3d8244b8
johnnybeatz <johnnybeatzxxd@gmail.com>
2024-03-08 20:14:44 +0300

    the play pause button chainging when changing the tab and come back Fixed

--- lib/components/playPauseButton.dart
+++ lib/components/playPauseButton.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:musicplayer_app/Providers/mainProvider.dart';
 import 'package:musicplayer_app/Providers/playgroundProvider.dart';
 import 'package:provider/provider.dart';
 import 'dart:async';
@@ -12,6 +13,7 @@ class PlayPause extends StatefulWidget {
 
 class _PlayPauseState extends State<PlayPause> with TickerProviderStateMixin {
   late AnimationController _controller;
+
   @override
   void initState() {
     _controller = AnimationController(
@@ -19,27 +21,24 @@ class _PlayPauseState extends State<PlayPause> with TickerProviderStateMixin {
     super.initState();
   }
 
+  refreshPage() {
+    print("no");
+    Provider.of<MainProvider>(context, listen: true).currentPage(2);
+  }
+
   @override
   void dispose() {
-    _controller.dispose();
+    //_controller.dispose();
+
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final musicPlayer = Provider.of<playGroundProvider>(context, listen: false);
     return GestureDetector(
         onTap: () {
-          final musicPlayer = Provider.of<playGroundProvider>(context, listen: false);
-          musicPlayer.changeIsPlay();
-          if (musicPlayer.isplay) {
-            _controller.forward();
-            musicPlayer.playTrack('https://cf-media.sndcdn.com/yJHZdaFxM1Xm.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20veUpIWmRhRnhNMVhtLjEyOC5tcDMqIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNzA5NjI5NzA3fX19XX0_&Signature=IB6GyPTf6kzoPndtI6tIfFIAiVMKnUSZwPLljo1lCQTIYZqoD-V1u-IG1Su1fgsHZ8jpS2RrMLowihoNeIQaaVzGo1qNPGULURG~40t9vJV0wP1qaGAGBJJjF-DogmbSkkOudmTJbYC5i3INixffIUoKKm3X8HGDcPxmfVP83nR5a4PPuAudftCE1nLDI6U9wAHwT5~Mj7mrknjYy5v7pXp4zghbcQX4dzHmBScLOgsK6WQxJZYB9vgZ6LAM7fuxSvrhc6RAVfBGSJ-Jt9oSQXE5jUto8CkQi1XV3ACayBD0oISitwT6Y-F03dRauwenszCrLKuxV5EgiXY~VkuCtA__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ',musicPlayer.position);
-          } else {
-            print(musicPlayer.isplay);
-            _controller.reverse();
-            musicPlayer.pauseTrack();
-            
-          }
+          _onTap(musicPlayer);
         },
         child: CircleAvatar(
             child: AnimatedIcon(
@@ -50,5 +49,16 @@ class _PlayPauseState extends State<PlayPause> with TickerProviderStateMixin {
             ),
             backgroundColor: const Color.fromRGBO(97, 86, 226, 1),
             radius: 40));
+  }
+
+  void _onTap(playGroundProvider musicPlayer) {
+    musicPlayer.changeIsPlay();
+    if (musicPlayer.isplay) {
+      _controller.forward();
+      musicPlayer.playTrack("url", musicPlayer.position);
+    } else {
+      _controller.reverse();
+      musicPlayer.pauseTrack();
+    }
   }
 }
