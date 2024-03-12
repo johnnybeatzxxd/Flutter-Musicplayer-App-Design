4b4cd002e0500ca9970c644612a0474ed1b3763c
johnnybeatz <johnnybeatzxxd@gmail.com>
2024-03-12 22:31:25 +0300

    used audio query package  and display the song name and artist name

--- lib/Pages/musics.dart
+++ lib/Pages/musics.dart
@@ -1,9 +1,10 @@
 import "package:flutter/material.dart";
 import "package:musicplayer_app/components/myicons.dart";
+import "package:on_audio_query/on_audio_query.dart";
 
 class Musics extends StatelessWidget {
-  const Musics({super.key});
-
+  Musics({super.key});
+  final _audioQuery = OnAudioQuery();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,13 +17,31 @@ class Musics extends StatelessWidget {
           )
         ],
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) => ListTile(
-          leading: const Icon(Icons.music_note),
-          title: (Text("Music No $index")),
-          subtitle: Text("Artist Name"),
-        ),
-        itemCount: 100,
+      body: FutureBuilder<List<SongModel>>(
+        future: _audioQuery.querySongs(
+            sortType: null,
+            orderType: OrderType.ASC_OR_SMALLER,
+            uriType: UriType.EXTERNAL_PRIMARY,
+            ignoreCase: true),
+        builder: (context, music) {
+          if (music.data!.isEmpty) {
+            return const Center(
+              child: Text("No Songs Found"),
+            );
+          }
+          if (music.hasData) {
+            return ListView.builder(
+              itemBuilder: (context, index) => ListTile(
+                leading: const Icon(Icons.music_note),
+                title: Text(music.data![index].displayNameWOExt),
+                subtitle: Text(music.data![index].artist.toString()),
+                trailing: const Icon(Icons.more_horiz),
+              ),
+              itemCount: 100,
+            );
+          }
+          return const CircularProgressIndicator();
+        },
       ),
     );
   }
