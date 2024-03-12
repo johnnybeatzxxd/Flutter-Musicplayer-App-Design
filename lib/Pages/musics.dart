64ffdca55461f95bdff5ef90624d1660268f2b01
johnnybeatz <johnnybeatzxxd@gmail.com>
2024-03-12 22:56:35 +0300

    made it to request nedded permission before drawing the page

--- lib/Pages/musics.dart
+++ lib/Pages/musics.dart
@@ -1,10 +1,33 @@
 import "package:flutter/material.dart";
 import "package:musicplayer_app/components/myicons.dart";
 import "package:on_audio_query/on_audio_query.dart";
+import "package:permission_handler/permission_handler.dart";
+import "package:provider/provider.dart";
+import "package:musicplayer_app/index.dart";
 
-class Musics extends StatelessWidget {
+class Musics extends StatefulWidget {
   Musics({super.key});
   final _audioQuery = OnAudioQuery();
+
+  @override
+  _MusicsState createState() => _MusicsState();
+}
+
+class _MusicsState extends State<Musics> {
+  @override
+  void initState() async {
+    super.initState();
+    var status = await Permission.storage.request();
+    if (status.isGranted) {
+      // Permission granted, proceed with the operation
+    } else if (status.isDenied) {
+      Provider.of<MainProvider>(context, listen: true).currentPage(0);
+    } else if (status.isPermanentlyDenied) {
+      // Permission permanently denied, open app settings
+      openAppSettings();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +41,7 @@ class Musics extends StatelessWidget {
         ],
       ),
       body: FutureBuilder<List<SongModel>>(
-        future: _audioQuery.querySongs(
+        future: widget._audioQuery.querySongs(
             sortType: null,
             orderType: OrderType.ASC_OR_SMALLER,
             uriType: UriType.EXTERNAL_PRIMARY,
