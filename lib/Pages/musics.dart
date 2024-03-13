5551a1b066946c1bb8b8d0b431a8f5519796ed63
johnnybeatz <johnnybeatzxxd@gmail.com>
2024-03-13 12:23:02 +0300

    changed the class name of the musics page and added button navbar

--- lib/Pages/musics.dart
+++ lib/Pages/musics.dart
@@ -5,67 +5,80 @@ import "package:permission_handler/permission_handler.dart";
 import "package:provider/provider.dart";
 import "package:musicplayer_app/index.dart";
 
-class Musics extends StatefulWidget {
-  Musics({super.key});
+class MusicsPage extends StatefulWidget {
+  MusicsPage({super.key});
   final _audioQuery = OnAudioQuery();
 
   @override
-  _MusicsState createState() => _MusicsState();
+  _MusicsPageState createState() => _MusicsPageState();
 }
 
-class _MusicsState extends State<Musics> {
-  @override
-  void initState() async {
-    super.initState();
-    var status = await Permission.storage.request();
-    if (status.isGranted) {
-      // Permission granted, proceed with the operation
-    } else if (status.isDenied) {
-      Provider.of<MainProvider>(context, listen: true).currentPage(0);
-    } else if (status.isPermanentlyDenied) {
-      // Permission permanently denied, open app settings
-      openAppSettings();
-    }
-  }
-
+class _MusicsPageState extends State<MusicsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Musics"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: FutureBuilder<List<SongModel>>(
-        future: widget._audioQuery.querySongs(
-            sortType: null,
-            orderType: OrderType.ASC_OR_SMALLER,
-            uriType: UriType.EXTERNAL_PRIMARY,
-            ignoreCase: true),
-        builder: (context, music) {
-          if (music.data!.isEmpty) {
-            return const Center(
-              child: Text("No Songs Found"),
-            );
-          }
-          if (music.hasData) {
-            return ListView.builder(
-              itemBuilder: (context, index) => ListTile(
-                leading: const Icon(Icons.music_note),
-                title: Text(music.data![index].displayNameWOExt),
-                subtitle: Text(music.data![index].artist.toString()),
-                trailing: const Icon(Icons.more_horiz),
-              ),
-              itemCount: 100,
-            );
-          }
-          return const CircularProgressIndicator();
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Musics"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Stack(children: [
+          FutureBuilder<List<SongModel>>(
+            future: _checkPermissionAndQuerySongs(),
+            builder: (context, music) {
+              if (music.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (music.hasData && music.data!.isEmpty) {
+                return const Center(
+                  child: Text("No Songs Found"),
+                );
+              } else if (music.hasData) {
+                return ListView.builder(
+                  itemBuilder: (context, index) => ListTile(
+                    leading: const Icon(Icons.music_note),
+                    title: Text(music.data![index].displayNameWOExt),
+                    subtitle: Text(music.data![index].artist.toString()),
+                    trailing: const Icon(Icons.more_horiz),
+                  ),
+                  itemCount: music.data!.length,
+                );
+              } else {
+                print(music.connectionState);
+                return const Center(
+                  child: Text("Error fetching songs"),
+                );
+              }
+            },
+          ),
+          ButtomNavBar()
+        ]));
+  }
+
+  Future<List<SongModel>> _checkPermissionAndQuerySongs() async {
+    var status = await Permission.storage.request();
+    print("request sent!");
+    if (status.isGranted) {
+      // Permission granted, proceed with the operation
+      return widget._audioQuery.querySongs(
+        sortType: null,
+        orderType: OrderType.ASC_OR_SMALLER,
+        uriType: UriType.EXTERNAL_PRIMARY,
+        ignoreCase: true,
+      );
+    } else if (status.isDenied) {
+      Provider.of<MainProvider>(context).currentPage(0);
+      return Future.error("Permission denied");
+    } else if (status.isPermanentlyDenied) {
+      // Permission permanently denied, open app settings
+      openAppSettings();
+      return Future.error("Permission permanently denied");
+    }
+    return Future.error("Unknown error");
   }
 }
